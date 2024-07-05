60582b4893a18b517c1ea482cf92266253f2ceb3
Tulsi26 <tulsi.dewangan@ssipmt.com>
2024-07-05 13:57:27 +0530

    add

---
+++ components/navbar.js
@@ -0,0 +1,307 @@
+let navbar=()=>{
+    return `
+
+    <div id="container">
+    <div>
+        <img id="tonavimg" src="https://img.gkbcdn.com/s3/bn/2205/1920x80-627652482b40c9219884b811.jpg" alt="">
+        <div id="secondnav">
+            <div id="second1">
+                <div id="iconmob"><i class="fa-thin fa-mobile"></i>
+                </div>
+                  
+                        <div class="dropdown">
+                            <span>Save BIG with Our app! <img id="img555" src="https://sugarbook.com/assets/img/down-arrow-light-grey.svg" alt=""> </span>
+                            <div class="dropdown-content">
+                            <p>Get exclusive app deals and discounts, download our app & SAVE</p>
+                            <div  id="barcode">
+                                <div>
+                                    <img src="https://content1.geekbuying.com/V1.4/en/images/index_images/android_app.png" alt="">
+                                </div>
+                                <div>
+                                    <div><img src="https://content1.geekbuying.com/V1.4/en/images/index_images/app_store.jpg" alt=""></div>
+                                    <div><img src="https://content1.geekbuying.com/V1.4/en/images/index_images/google_play.jpg" alt=""></div>
+                                    <div><img src="https://content1.geekbuying.com/V1.4/en/images/index_images/gallery.jpg" alt=""></div>
+                                    
+                                  
+                                </div>
+                                
+                            </div>
+                            <h3>Go to mobile site</h3>
+                            <!-- <img src="https://m.media-amazon.com/images/I/51yY07ZOhHL._AC_SY450_.jpg" alt=""> -->
+                            </div>
+                          </div>
+
+                   
+                    <!--  -->
+                    
+                   
+                    <!-- <div>Language </div> -->
+                    <div class="dropdown">
+                        <span>Language <img id="img555" src="https://sugarbook.com/assets/img/down-arrow-light-grey.svg" alt=""></span>
+                        <div class="dropdown-content1">
+                            <p>English</p>
+                        <p>Italiano</p>
+                        <p>Deutsch</p>
+                        <p>Español</p>
+                        <p>Français</p>
+                        <p>Português</p>
+                        <p>Slovenčina</p>
+                        <p>Magyar</p>
+                        <p>Slovenščina</p>
+                        <p>Lietuvių kalba</p>
+                        <p>Svenska</p>
+                        <p>Ελληνικά</p>
+                        <p>Dansk</p>
+                        <p>עברית</p>
+                        <p>Hrvatski jezik</p>
+                        <p>Polskie</p>
+                        <p>Limba română</p>
+                        <p>Čeština</p>
+                        <p>Japanese</p>
+
+
+                        </div>
+                      </div>
+
+
+                      
+                     
+                    <div id="navimg">
+
+                        
+                        <!-- <div>ship to</div> -->
+                        <div class="dropdown">
+                            <span>Ship to</span>
+                            <div class="dropdown-content">
+                           <h3>Select Regional Settings</h3>
+                           <p>Ship to</p>
+                          
+                           <div id="country_select">
+                               <img src="https://m.media-amazon.com/images/I/51yY07ZOhHL._AC_SY450_.jpg" alt="">
+                               <p>India</p>
+                               <select  class="dropbtn1" name="">
+                                <br>
+                                   
+                                   <option value="">India</option>
+                                   <option value="">United state</option>
+                                   <option value="">United Kindom</option>
+                                   <option value="">Itley</option>
+                                   <option value="">France</option>
+                                   <option value="">Span</option>
+                                   <option value="">German</option>
+                                   <option value="">Japan</option>
+                                   <option value="">China</option>
+                                   <option value="">Erope</option>
+
+                               </select>
+                           </div>
+                           <p>Currency</p>
+                           <div id="currency">
+                               <p>INR=</p>
+                               <select name="" id="">
+                                   <div id="doller">
+                                       <div>
+                                           <p>Ruppes</p>
+                                       </div>
+                                       <div></div>
+                                   </div>
+                               </select>
+
+                           </div>
+                           <button id="save">Save</button>
+
+
+                            </div>
+                          </div>
+                        <div><img src="https://m.media-amazon.com/images/I/51yY07ZOhHL._AC_SY450_.jpg" alt=""></div>
+                      
+                        <div style="font-size="13px"> / INR <img style="width: 1.1rem;height: 1.1rem;margin-top:-.8rem" src="https://sugarbook.com/assets/img/down-arrow-light-grey.svg" alt=""></div>
+
+                     
+                        
+                    </div>
+
+                    <!-- <div> Support Center </div> -->
+                    <div class="dropdown">
+                        <span>Support Center <img id="img555" src="https://sugarbook.com/assets/img/down-arrow-light-grey.svg" alt=""></span>
+                        <div class="dropdown-content">
+                            <div>
+                                <i class="fa-brands fa-rocketchat"></i>
+                                <p> Live Chat</p>
+                            </div>
+                            <div>
+                                <i class="fa-brands fa-rocketchat"></i>
+                                <!-- <i class="fa-solid fa-arrow-up-right"></i> -->
+                                <p>Ticket</p>
+                            </div>
+                            <div>
+                                <i class="fa-brands fa-rocketchat"></i>
+                                <!-- <i class="fa-solid fa-message-question"></i> -->
+                                <p>Help Centre</p>
+                            </div>
+                       
+                        </div>
+                      </div>
+            </div>
+
+        </div>
+        <hr>
+
+    </div>
+    
+    <div id="hr1">
+        <hr>
+    </div>
+   
+
+    <div id="navbartwo">
+        <div onclick="home()"><img src="https://cdn.megabonus.com/images/shop_logo/origin/geekbuyingoptimizedcom_shop_594.png" alt=""></div>
+       
+        <div class="dropdown2">
+             <button onclick="myFunction()" class="dropbtn">All Categories</button>
+            <div id="myDropdown" class="dropdown-content2">
+            <div id="down">
+                <div>
+                    <a href="#">All Categories</a>
+                    <a href="#">Smart Home & Garden</a>
+                    <a href="#">Phones & Accessories</a>
+                    <a href="#">Computers, Tablets & Accessories</a>
+                    <a href="#">Wearable Devices</a>
+                    <a href="#">Automobiles & Motorcycles</a>
+                 
+                 </div>
+                 <div>
+                    <a href="#">Sports & Outdoors</a>
+                    <a href="#">Consumer Electronics</a>
+                    <a href="#">TV Boxes & Mini PCs</a>
+                    <a href="#">Toys & Hobbies</a>
+                    <a href="#">Security System</a>
+                    <a href="#">Fashion</a>
+                   
+                   
+                 </div>
+                 
+
+            </div>
+            </div>
+          </div>
+
+          <!-- search_bar -->
+         
+
+
+          <div>
+               <input type="text" id="query" placeholder="search by keyboards">
+          </div>
+          <div>
+                 <i class="fa-solid fa-magnifying-glass search"></i>
+          </div>
+
+          <a class="decarution" href="SignIn.html">
+          <div id="user">
+              <i class="fa-regular fa-user"></i>
+          
+            <p>sign in</p>
+        
+        </div></a>
+
+        <a class="decarution" href="wishlistPage.html">
+        <div id="wish">
+        <i class="fa-regular fa-heart"></i>
+       
+           
+           <p class='totalItemInWish'>wishlist</p>
+       </div>
+        </a>
+
+       
+
+        <a class="decarution" href="shoppingcart.html">
+        <div id="cart">
+        <i  class="fa-solid fa-bag-shopping"></i>
+        <div><p id="show" class'totalItemInCart'>0</p>
+            <p >Cart</p>
+        </div>
+    </div></a>
+
+     
+          
+    </div>
+
+
+    <div id="shop">
+        <div class="dropdown3">
+            <span>Shop All Categories <i class="fa-solid fa-caret-down"></i></span>
+            <div class="dropdown-content3">
+            <!-- <p>Local Warehouses</p> -->
+
+          <!-- <div id="drp4"> -->
+            <div class="dropdown4">
+                <span>Local Warehouses</span>
+                <div class="dropdown-content4">
+                    <div id="drp4"> 
+                        <div>
+                            <h4>EU Warehouse</h4>
+                        <p class="fade">EU Deals</p>
+                        <h4>DE Warehouse</h4>
+                        <p class="fade">DE Deals</p>
+                        <h4>PL Warehouse</h4>
+                        <h4>ES Warehouse</h4>
+                        <p class="fade">EU Deals</p>
+                        <h4>EU Warehouse</h4>
+                        <p class="fade">ES Deals</p>
+    
+                        <h4>CZ Warehouse</h4>
+                      
+                        <h4>UK Warehouse</h4>
+                        
+                        <h4>EU-3 Warehouse</h4>
+                        </div>
+                        <div>
+                            <img id="map" src="https://img.gkbcdn.com/s3/cim/category-1301-6ETJOmbg.png" alt="">
+                        </div>
+                    </div>
+             
+               
+              
+                </div>
+            
+              </div>
+              
+          <!-- </div> -->
+
+
+            <p>Smart Home & Garden</p>
+            <p>Sports & Outdoors</p>
+            <p>Consumer Electronics</p>
+            <p>TV Boxes & Mini PCs</p>
+            <p>Computers, Tablets & Accessories</p>
+            <p>Toys & Hobbies</p>
+            <p>Phones & Accessories</p>
+            <p>Automobiles & Motorcycles</p>
+            <p>Wearable Devices</p>
+            <p>Security Systems</p>
+            <p>Fashion</p>
+            </div>
+     
+          
+          </div>
+          <div>
+            <div id="new">
+                <a href="new.html">New</a>
+            <a href="top-seller.html">Top seller</a>
+            <a href="https://www.geekbuying.com/top_brand_at_geekbuying/">Brand</a>
+            <a href="#">Clearance</a>
+            <a href="#">Deals</a>
+            <a href="#"> Bargain</a>
+            <a href="coupon.html">Coupon</a>
+          </div>
+          </div>
+    </div>
+
+</div>
+    
+    `
+}
+
+export{navbar}
---
+++ printer/script/rob.js
@@ -0,0 +1,242 @@
+var robData=[
+
+{
+    image_url: ["https://img.gkbcdn.com/p/2022-04-24/artillery-sidewinder-x2-3d-printer-3b98f7-1650788405823._w280_.png",
+   "https://img.gkbcdn.com/p/2022-03-02/creality-ender-3-s1-pro-3d-printer-c9e30b-1646215071933._w60_p1_.jpg",
+   "https://img.gkbcdn.com/p/2022-03-02/creality-ender-3-s1-pro-3d-printer-c5a489-1646215071949._w60_p1_.jpg",
+   "https://img.gkbcdn.com/p/2022-03-02/creality-ender-3-s1-pro-3d-printer-436cb1-1646215071933._w60_p1_.jpg",
+   "https://img.gkbcdn.com/p/2022-03-02/creality-ender-3-s1-pro-3d-printer-698915-1646215071965._w60_p1_.jpg",
+   "https://img.gkbcdn.com/p/2021-09-16/Artillery-Sidewinder-X2-3D-Printer-472515-3._w60_p1_.jpg",
+],
+    name: "Artillery Sidewinder X2 3D Printer",
+    price: "₹11099.94",
+    strikedoffprice: "₹12728.92",
+},
+{
+    image_url:[ "https://img.gkbcdn.com/p/2021-09-17/Anycubic-Vyper-3D-Printer-472768-0._w280_p1_.jpg",
+    "https://img.gkbcdn.com/p/2022-03-02/creality-ender-3-s1-pro-3d-printer-c9e30b-1646215071933._w60_p1_.jpg",
+    "https://img.gkbcdn.com/p/2022-03-02/creality-ender-3-s1-pro-3d-printer-c5a489-1646215071949._w60_p1_.jpg",
+    "https://img.gkbcdn.com/p/2022-03-02/creality-ender-3-s1-pro-3d-printer-436cb1-1646215071933._w60_p1_.jpg",
+    "https://img.gkbcdn.com/p/2022-03-02/creality-ender-3-s1-pro-3d-printer-698915-1646215071965._w60_p1_.jpg",
+    "https://img.gkbcdn.com/p/2021-09-16/Artillery-Sidewinder-X2-3D-Printer-472515-3._w60_p1_.jpg",
+ ],
+    name: "Creality Ender-3 S1 Pro 3D Printer ",
+    price: "₹3099.94",
+    strikedoffprice: "₹4728.92",
+},
+{
+    image_url:[ "https://img.gkbcdn.com/p/2021-10-11/Anycubic-Wash-and-Cure-2-0-Machine-473349-0._w280_p1_.jpg",
+   "https://img.gkbcdn.com/p/2021-10-11/Anycubic-Wash-and-Cure-2-0-Machine-473349-0._w60_p1_.jpg",
+   "https://img.gkbcdn.com/p/2021-10-11/Anycubic-Wash-and-Cure-2-0-Machine-473349-2._w60_p1_.jpg",
+   "https://img.gkbcdn.com/p/2021-10-11/Anycubic-Wash-and-Cure-2-0-Machine-473349-6._w60_p1_.jpg",
+   "https://img.gkbcdn.com/p/2021-10-11/Anycubic-Wash-and-Cure-2-0-Machine-473349-1._w60_p1_.jpg",
+   "https://img.gkbcdn.com/p/2021-10-11/Anycubic-Wash-and-Cure-2-0-Machine-473349-5._w60_p1_.jpg",
+],
+    name: "Xiaomi Mi Robot Vacuum Cleaner Robot Virtual Wall for",
+    price: "₹1099.94",
+    strikedoffprice: "₹1728.92",
+},
+{
+    image_url:[ "https://img.gkbcdn.com/p/2021-09-16/Artillery-Genius-Pro-3D-Printer-472514-0._w280_p1_.jpg",],
+    name: "Robot Vacuum Cleaner Robot Virtual Wall for",
+    price: "₹10099.94",
+    strikedoffprice: "₹16728.92",
+},
+{
+    image_url: ["https://img.gkbcdn.com/p/2021-07-12/Artillery-Hornet-2021-Newest-Model-3D-Printer-with-Metal-Integrated-St-461282-0._w280_p1_.jpg",
+  
+  ],
+    name: "Proscenic 850T Printer Cleaner 3000Pa Suction Three  ",
+    price: "₹12099.94",
+    strikedoffprice: "₹31728.92",
+},
+{
+    image_url:[ "https://img.gkbcdn.com/p/2021-12-20/Makibes-3D-Printer-1Kg-PLA-Black-481859-1._w280_p1_.jpg",],
+    name: "Xiaomi Mi Robot Vacuum Cleaner Robot Virtual Wall for",
+    price: "₹10099.94",
+    strikedoffprice: "₹17280",
+},
+{
+    image_url: ["https://img.gkbcdn.com/p/2021-10-11/Anycubic-Wash-and-Cure-2-0-Machine-473349-0._w280_p1_.jpg",],
+    name: " Vacuum Cleaner Robot Virtual Wall for",
+    price: "₹2099.94",
+    strikedoffprice: "₹7728.92",
+},
+{
+    image_url:[ "https://img.gkbcdn.com/p/2022-01-22/ELEGOO-Mars-2-Pro-MSLA-Resin-3D-Printer-494877-0._w280_p1_.jpg",],
+    name: "Xiaomi Mi Robot Vacuum Cleaner Robot Virtual Wall for",
+    price: "₹1099.94",
+    strikedoffprice: "₹1728.92",
+},
+{
+    image_url:[ "https://img.gkbcdn.com/p/2021-09-17/Anycubic-Vyper-3D-Printer-472768-0._w280_p1_.jpg",],
+    name: "Printer Virtual Wall for",
+    price: "₹3099.94",
+    strikedoffprice: "₹5728.92",
+},
+
+{
+    image_url: ["https://img.gkbcdn.com/p/2021-09-16/Artillery-Genius-Pro-3D-Printer-472514-0._w280_p1_.jpg",],
+    name: "Robot Vacuum Cleaner Robot Virtual Wall for",
+    price: "₹10099.94",
+    strikedoffprice: "₹16728.92",
+},
+{
+    image_url: ["https://img.gkbcdn.com/p/2021-07-12/Artillery-Hornet-2021-Newest-Model-3D-Printer-with-Metal-Integrated-St-461282-0._w280_p1_.jpg",
+],
+    name: "Proscenic 850T Printer Cleaner 3000Pa Suction Three  ",
+    price: "₹12099.94",
+    strikedoffprice: "₹31728.92",
+},
+{
+    image_url:[ "https://img.gkbcdn.com/p/2021-12-20/Makibes-3D-Printer-1Kg-PLA-Black-481859-1._w280_p1_.jpg",],
+    name: "Xiaomi Mi Robot Vacuum Cleaner Robot Virtual Wall for",
+    price: "₹10400",
+    strikedoffprice: "₹15000",
+},
+{
+    image_url:[ "https://img.gkbcdn.com/p/2021-10-11/Anycubic-Wash-and-Cure-2-0-Machine-473349-0._w280_p1_.jpg",],
+    name: " Vacuum Cleaner Robot Virtual Wall for",
+    price: "₹20000",
+    strikedoffprice: "₹23489",
+},
+{
+    image_url:[ "https://img.gkbcdn.com/p/2022-01-22/ELEGOO-Mars-2-Pro-MSLA-Resin-3D-Printer-494877-0._w280_p1_.jpg",],
+    name: "Xiaomi Mi Robot Vacuum Cleaner Robot Virtual Wall for",
+    price: "₹10005",
+    strikedoffprice: "₹14007",
+},
+{
+    image_url: ["https://img.gkbcdn.com/p/2021-09-17/Anycubic-Vyper-3D-Printer-472768-0._w280_p1_.jpg",],
+    name: "Printer Virtual Wall for",
+    price: "₹30000",
+    strikedoffprice: "₹57000",
+},
+
+
+
+
+
+];
+
+
+
+robData.map(function (el) {
+    
+    var box=document.createElement("div")
+
+    let img =document.createElement("img")
+    img.src=el.image_url[0];
+    img.setAttribute("id","img123")
+    img.addEventListener("click",function(){
+        imageclick(el);
+    })
+    
+
+
+    let name=document.createElement("p")
+    name.textContent=el.name
+
+
+    let price=document.createElement("h4")
+price.innerText=el.price;
+
+    let strikedoffprice=document.createElement("p")
+    strikedoffprice.innerText=el.strikedoffprice
+    strikedoffprice.style.textDecoration = "line-through";
+    
+    var btn = document.createElement("icon");
+    
+    btn.setAttribute("class","fas fa-heart");
+    btn.setAttribute("id","wishheart");
+    btn.style.color = "grey"
+    btn.style.marginLeft="20px"
+    // btn.innerText = "Add Card";
+    // btn.style.textAlign="center"
+    // btn.style.backgroundColor="teal"
+
+    btn.addEventListener("click", function () {
+        addToCart(el);
+        if(btn.style.color = "grey" ){
+            btn.style.color = "red";
+            count.innerText = "1";
+        }
+        else if(btn.style.color = "grey"){
+            count.innerText = "0";
+        }
+      });
+
+    box.append(img, name, price,strikedoffprice,btn);
+
+    document.querySelector("#image_section").append(box);
+
+
+    
+
+});
+
+function imageclick(el){
+    var robData =  [];
+//   alert("Item Successfully Added To Your Cart")
+  robData.push(el);
+  localStorage.setItem("cart",JSON.stringify(robData))
+  window.location.href = "add_to_cart.html"
+}
+
+
+function addToCart(el) {
+    let arr=JSON.parse(localStorage.getItem('wish'))
+    console.log('el',el);
+    // robotData.push(el);
+    arr.push(el)
+    localStorage.setItem("wish", JSON.stringify(arr));
+    totalItemInWishFun();
+    alert("Item Successfully Added To Your wish")
+    // window.location.href="cart.html"
+  }
+
+
+
+  let totalItemInWishFun=()=>{
+    let totalItemInWish=document.querySelector('.totalItemInWish')
+
+    let wishItem=JSON.parse(localStorage.getItem('wish'))
+
+    totalItemInWish.innerHTML=wishItem.length;
+
+    totalItemInWish.style.backgroundColor='yellow'
+
+    totalItemInWish.style.color='red'
+
+    totalItemInWish.style.fontWeight='bold'
+    
+
+
+}
+
+totalItemInWishFun();
+
+
+
+
+let totalItemInCartFun=()=>{
+    let totalItemIncart=document.querySelector('.totalItemInCart')
+
+    let cartItem=JSON.parse(localStorage.getItem('cartData'))
+
+    console.log(cartItem)
+    totalItemIncart.innerHTML=cartItem.length;
+
+    totalItemIncart.style.backgroundColor='yellow'
+
+    totalItemIncart.style.color='red'
+
+    totalItemIncart.style.fontWeight='bold'
+    
+
+
+}
+
+totalItemInCartFun();
+
+
